5481a7094263f15c7b0ca97ce273de3f2d3d3788
Yoshi <yoshi@mac-mini.local>
2021-12-11 23:39:59 +0900

    add new

---
+++ app/routes/jokes/new.tsx
@@ -0,0 +1,26 @@
+function NewJokeRoute() {
+  return (
+    <div>
+      <p>Add your own joke</p>
+      <form action="post">
+        <div>
+          <label>
+            Name: <input type="text" name="name" />
+          </label>
+        </div>
+        <div>
+          <label>
+            Content: <textarea name="content" />
+          </label>
+        </div>
+        <div>
+          <button type="submit" className="button">
+            Add
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default NewJokeRoute;
